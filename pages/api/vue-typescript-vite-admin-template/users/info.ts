103443a4e232c157f35fd374f0033293e2e5dc29
LLawlight <me@zhaoyuxiang.cn>
2021-04-09 20:12:05 +0800

    feat: vue-admin-template mock api

---
+++ pages/api/vue-typescript-vite-admin-template/users/info.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (_: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({
+    code: 20000,
+    data: {
+      user: {
+        roles: ["admin"],
+        name: "vue-typescript-vite-admin",
+        avatar:
+          "https://zhaoyuxiang.cn/_next/image?url=%2Fimages%2Fprofile.jpg&w=256&q=75",
+        introduction: "https://zhaoyuxiang.cn",
+      },
+    },
+  });
+};
