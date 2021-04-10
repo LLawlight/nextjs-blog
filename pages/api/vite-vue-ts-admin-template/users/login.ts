ce9bb1101895b03cf2595bd9c35b3c4d397e7ad9
天道总司 <me@zhaoyuxiang.cn>
2021-04-10 11:40:55 +0800

    chore: change mock api data

--- pages/api/vite-vue-ts-admin-template/users/login.ts
+++ pages/api/vite-vue-ts-admin-template/users/login.ts
@@ -4,7 +4,7 @@ export default (_: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({
     code: 20000,
     data: {
-      accessToken: "vue-typescript-vite-admin-template_token",
+      accessToken: "vite-vue-ts-admin-template_token",
     },
   });
 };
